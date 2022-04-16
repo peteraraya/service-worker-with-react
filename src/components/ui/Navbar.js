09a4163a885dc6faea5403e6f57dc0caa6e7ea09
parayaticblue <piteraraya@gmail.com>
2022-04-16 17:59:34 -0400

    se añade optimización a configuración de workbox y se agrega componente de online y offline

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { startLogout } from '../../actions/auth';
+import  { Offline, Online } from 'react-detect-offline';
 
 export const Navbar = () => {
 
@@ -16,7 +17,12 @@ export const Navbar = () => {
             <span className="navbar-brand">
                 { name }
             </span>
-            
+            <Online>
+                <span className='text-success'>Online</span>
+            </Online>
+            <Offline>
+                <span className='text-danger'>Offline - Peticiones seran guardadas</span>
+            </Offline>
             <button 
                 className="btn btn-outline-danger"
                 onClick={ handleLogout }
--- src/sw-template.js
+++ src/sw-template.js
@@ -12,41 +12,47 @@ const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
 const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
-
+// *** Optimización  *** //
+const cacheNetworkFirst = [
+  '/api/auth/renew',
+  '/api/events'
+]
+// *** Optimización  *** //
+const cacheFirstMetwork = [
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'
+]
 
 
 // Añadimos archivos estaticos
 registerRoute( 
-  new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
-  // Aplicando la estrategoia de revisar el cache primero 
+ ({request,url}) => {
+    // console.log({ request, url });
+  if (cacheFirstMetwork.includes(url.href)) return true;
+  return false;
+  },
   new CacheFirst()
 )
-registerRoute( 
-  new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
-  // Aplicando la estrategoia de revisar el cache primero 
-  new CacheFirst()
-)
-
 
 // se utilizara la estrategia network first para cualquier archivo que no este en el cache
+// *** Optimización  *** //
+registerRoute(
+  ({request,url}) => {
+    // console.log({ request, url });
+    if (cacheNetworkFirst.includes(url.pathname)) return true;
 
-registerRoute(
-  new RegExp('http://localhost:4000/api/auth/renew'),
-  new NetworkFirst()
-)
-registerRoute(
-  new RegExp('http://localhost:4000/api/events'),
+    return false;
+  },
   new NetworkFirst()
 )
 
 
 // Posteos Offline
-
 const bgSyncPlugin = new BackgroundSyncPlugin('posteos offline', {
   maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
 });
 registerRoute(
-  new RegExp('http://localhost:4000/api/events'),
+  new RegExp('http://localhost:4000/api/events/'),
   new NetworkOnly({
     plugins: [bgSyncPlugin],
   }),
@@ -55,7 +61,6 @@ registerRoute(
 
 
 // Delete Online
-
 registerRoute(
   new RegExp('http://localhost:4000/api/events/'),
   new NetworkOnly({
